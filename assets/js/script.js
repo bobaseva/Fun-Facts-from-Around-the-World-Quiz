58e0ad454bc8efa48857de12c574e967af71b34f
bobaseva <>
2023-10-03 19:20:11 +0000

    Declare game and controls variables

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const introSection = document.querySelector(".intro");
+const formSection = document.querySelector(".user");
+const gameSection = document.querySelector(".game-play");
+const resultSection = document.querySelector(".result");
+let modal = document.querySelector(".modal");
+let modalButton = document.getElementById("modal-button");
+let closeButton = document.querySelector(".close-button");
+
+const usernameP = document.getElementById("username-output");
+const userName = document.getElementById("username");
+
+const questionElement = document.getElementById("facts");
+const optionElements = document.querySelectorAll(".options");
+const allAnswers = document.getElementById("answer-buttons");
+const nextBtn = document.querySelector(".next");
