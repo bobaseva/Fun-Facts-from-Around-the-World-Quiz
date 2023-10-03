d11c570f5a2e4f439f9d118e0e09057149bb0fc5
bobaseva <>
2023-10-03 19:58:58 +0000

    Add all 10 questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,6 +1,54 @@
 const questions = [
     {
-        text: "",
-        options: [],
-        answer: ""
+        text: "1. Finland has been rated as what type of Country for the last 3 years running (2020, 2019 & 2018)?",
+        options: ["The coldest Country in the world", "The happiest Country in the world", "The most boring Country in the world"],
+        answer: "The happiest Country in the world"
     },
+    {
+        text: "2. Who has more Pyramids, Egypt or Sudan?",
+        options: ["Egypt", "Sudan", "Mexico"],
+        answer: "Sudan"
+    },
+    {
+        text: "3. Which city has more cars than it does people?",
+        options: ["Monaco City, Monaco", "Male, Maldives", "San Marino, Italy"],
+        answer: "San Marino, Italy"
+    },
+    {
+        text: "4. What is Scotlands National Animal?",
+        options: ["Unicorn", "Highland Cow", "Squirrel"],
+        answer: "Unicorn"
+    },
+    {
+        text: "5. The Book ‘Shantaram’ is based in which city in India?",
+        options: ["New Delhi", "Mumbai", "Kolkata"],
+        answer: "Mumbai"
+    },
+    {
+        text: "6. What is considered the happiest language in the world?",
+        options: ["English", "Italian", "Spanish"],
+        answer: "Spanish"
+    },
+    {
+        text: "7. In which country is giving a thumbs up an insult?",
+        options: ["Estonia", "India", "Iran"],
+        answer: "Iran"
+    },
+    {
+        text: "8. Which country invented tea?",
+        options: ["India", "Japan", "China"],
+        answer: "China"
+    },
+    {
+        text: "9. Which is the driest continent?",
+        options: ["Asia", "Antactica", "Africa"],
+        answer: "Antarctica"
+    },
+    {
+        text: "10. How many different spoken languages exist in the world today?",
+        options: ["6,500 languages", "1,000 languages", "9,000 languages"],
+        answer: "6,500 languages"
+    }
+];
+
+
