4697917ea9f6847cfc50163e7b48c3f0c5d9daab
bobaseva <>
2023-10-03 19:26:50 +0000

    Add functions for Name Form and game controls

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,35 @@ function showHideIntro() {
     formSection.classList.remove("hide");
 }
 
+function enterBtn() {
+    console.log("function fired");
+
+    let userName = document.getElementById("username").value;
+
+    if (userName === null || userName === "") {
+        let errorDiv = document.getElementById("errors");
+        errorDiv.innerHTML = "Please choose a username to play the game!";
+        errorDiv.style.display = "block";
+
+    } else {
+        console.log("Username entered successfully!");
+        formSection.classList.add("hide");
+        gameSection.classList.remove("hide");
+    }
+}
+
+function displayUsername() {
+
+    usernameP.innerHTML = userName.value + "!";
+    console.log("Username:", userName.value);
+}
+
+function hideGame() {
+    console.log("Game hidden");
+
+    questionElement.classList.add("hide");
+    nextBtn.classList.add("hide");
+    allAnswers.style.display = "none";
+}
+
+
